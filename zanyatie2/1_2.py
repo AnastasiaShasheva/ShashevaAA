35263f23878fbb56e541de6f21f328f2f888423f
AnastasiaShasheva <shashevanastya2600@gmail.com>
2021-09-29 18:28:05 +0300

    cho

--- zanyatie2/1_2.py
+++
@@ -1,7 +0,0 @@
-string.encode(encoding='UTF-8',errors='strict')
-
-print("Первое задание")
-a = int(input("Первое число: "))
-b = int(input("Второе число: "))
-c = int(input("Третье число: "))
-print("Сумма" + str(a+b+c) + "\n")
--- zanyatie2/2_2.py
+++
@@ -1,6 +0,0 @@
-string.encode(encoding='UTF-8',errors='strict')
-
-print("Второе задание")
-a = int(input("Первый катет: "))
-b = int(input("Второй катет: "))
-print("Площадь: " + str(a*b/2) + "\n")
