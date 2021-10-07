b95e08b25f39289faa97b32b628c05c4c1aaea42
Настя <90566758+AnastasiaShasheva@users.noreply.github.com>
2021-10-07 11:46:08 +0300

    Update 8.py

--- zanyatie2/8.py
+++ zanyatie2/8.py
@@ -8,6 +8,8 @@ def chis(f, s, t):
 		for n in range(0, 3):
 			if (arr1[i] == arr1[n]) and (n != i):
 				k += 1
+				if k == 6:
+                                        k /= 2
 	return k
-arr = input("Введите три числа").split()
+arr = input("Введите три числа: ").split()
 print(str(chis(arr[0], arr[1], arr[2])) + "\n")
